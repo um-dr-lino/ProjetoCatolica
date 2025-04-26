bddefa1fef3529d4b26d0bcd3436d889252852db
ALYSSON LINO <alysson.lino@catolicasc.edu.br>
2025-04-25 21:59:44 -0300

    Inicio projeto 25.04

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿Tela tela = new Tela(80,25,ConsoleColor.Green, ConsoleColor.Yellow);
+int opcao;
+
+
+while(true){
+    /*
+    Preparar o menu com opções de CRUD do cliente
+    Usuario escolhe uma opção
+    executar a opção escolhida na classe cliente
+    */
+    tela.desenhaMoldura(0,0,79,24);
+    tela.desenhaMoldura(1,1,15,10);
+    tela.desenhaMoldura(17,5,70,20);
+    Console.ReadKey();
+}
---
+++ ConsoleApp1/ConsoleApp1/Tela.cs
@@ -0,0 +1,45 @@
+public class Tela{
+    private int largura;
+    private int altura;
+    private ConsoleColor corFundo;
+    private ConsoleColor corTexto;
+
+    public Tela(int largura, int altura, ConsoleColor fundo, ConsoleColor texto) {
+        this.largura    = largura;
+        this.altura     = altura; 
+        this.corFundo   = fundo;
+        this.corTexto   = texto;
+    }
+
+    
+    //metodo para preparar a tela 
+
+
+    //--====metodo para desenhar molduras===--//
+    /*O primeiro elemento sempre é a coluna e o segundo sempre é a linha*/
+    public void desenhaMoldura(int colIni, int linIni, int colFin, int linFin){
+        //linhas horizontais 
+        for(int x=colIni; x<= colFin; x++){
+            Console.SetCursorPosition(x, linIni);
+            Console.Write("-");
+            Console.SetCursorPosition(x, linFin);
+            Console.Write("-");
+        }	
+        //linhas verticais
+        for(int y =linIni; y<= linFin; y++){
+            Console.SetCursorPosition(colIni, y);
+            Console.Write("|");
+            Console.SetCursorPosition(colFin, y);
+            Console.Write("|");
+        }	
+        //cantos 
+        Console.SetCursorPosition(colIni,linIni); Console.Write("+");
+        Console.SetCursorPosition(colIni,linFin); Console.Write("+");
+        Console.SetCursorPosition(colFin,linIni); Console.Write("+");
+        Console.SetCursorPosition(colFin,linFin); Console.Write("+");
+    }
+    //metodo para centralizar titulos
+
+    //metodo para mostrar um menu 
+ 
+}
