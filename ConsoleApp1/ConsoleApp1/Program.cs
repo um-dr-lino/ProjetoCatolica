c2978b5d0d7adf6344c67d64e27ee58276469842
ALYSSON LINO <alysson.lino@catolicasc.edu.br>
2025-05-09 19:22:02 -0300

    Projeto 09.05

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,13 +1,39 @@
-﻿Tela tela = new Tela(70,15,ConsoleColor.Black, ConsoleColor.Yellow);
-int opcao;
+﻿List<string> opcoes = new List<string>();
+opcoes.Add("1 - Manter Clientes");
+opcoes.Add("2 - Manter Temas");
+opcoes.Add("3 - Registrar Aluguel");
+opcoes.Add("4 - Registrar Devolução");
+opcoes.Add("0 - Sair");
 
+Tela tela = new Tela(80, 25, ConsoleColor.Black, ConsoleColor.Yellow);
+string opcao;
 
-while(true){
-    /*
-    Preparar o menu com opções de CRUD do cliente
-    Usuario escolhe uma opção
-    executar a opção escolhida na classe cliente
-    */
-    tela.prepararTela();
+while (true)
+{
+    tela.prepararTela("Fiesta!!!");
+    opcao = tela.mostrarMenu(opcoes, 1, 3);
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Manter Clientes");
+            break;
+        case "2":
+            Console.WriteLine("Manter Temas");
+            break;
+        case "3":
+            Console.WriteLine("Registrar Aluguel");
+            break;
+        case "4":
+            Console.WriteLine("Registrar Devolução");
+            break;
+        case "0":
+            Console.WriteLine("Saindo...");
+            return;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+
+    Console.WriteLine("Pressione qualquer tecla para continuar...");
     Console.ReadKey();
 }
--- ConsoleApp1/ConsoleApp1/Tela.cs
+++ ConsoleApp1/ConsoleApp1/Tela.cs
@@ -1,50 +1,100 @@
-public class Tela{
+public class Tela
+{
     private int largura;
     private int altura;
     private ConsoleColor corFundo;
     private ConsoleColor corTexto;
 
-    public Tela(int largura, int altura, ConsoleColor fundo, ConsoleColor texto) {
-        this.largura    = largura;
-        this.altura     = altura; 
-        this.corFundo   = fundo;
-        this.corTexto   = texto;
+
+    public Tela(int largura, int altura, ConsoleColor fundo, ConsoleColor texto)
+    {
+        this.largura = largura;
+        this.altura = altura;
+        this.corFundo = fundo;
+        this.corTexto = texto;
     }
 
-    
-    //metodo para preparar a tela 
-    public void prepararTela(){
+
+    public void prepararTela(string titulo = "")
+    {
         Console.ForegroundColor = corTexto;
         Console.BackgroundColor = corFundo;
         Console.Clear();
-        this.desenhaMoldura(0,0,this.largura,this.altura);
+        this.desenharMoldura(0, 0, this.largura, this.altura);
+        this.desenharMoldura(0, 0, this.largura, 2);
+        this.centralizar(titulo, 1, 0, this.largura);
     }
 
-    //--====metodo para desenhar molduras===--//
-    /*O primeiro elemento sempre é a coluna e o segundo sempre é a linha*/
-    public void desenhaMoldura(int colIni, int linIni, int colFin, int linFin){
-        //linhas horizontais 
-        for(int x=colIni; x<= colFin; x++){
+
+    public void desenharMoldura(int colIni, int linIni, int colFin, int linFin)
+    {
+        // linhas horizontais
+        for (int x = colIni; x <= colFin; x++)
+        {
             Console.SetCursorPosition(x, linIni);
             Console.Write("-");
             Console.SetCursorPosition(x, linFin);
             Console.Write("-");
-        }	
-        //linhas verticais
-        for(int y =linIni; y<= linFin; y++){
+        }
+        // linhas verticais
+        for (int y = linIni; y <= linFin; y++)
+        {
             Console.SetCursorPosition(colIni, y);
             Console.Write("|");
             Console.SetCursorPosition(colFin, y);
             Console.Write("|");
-        }	
-        //cantos 
-        Console.SetCursorPosition(colIni,linIni); Console.Write("+");
-        Console.SetCursorPosition(colIni,linFin); Console.Write("+");
-        Console.SetCursorPosition(colFin,linIni); Console.Write("+");
-        Console.SetCursorPosition(colFin,linFin); Console.Write("+");
+        }
+        // cantos
+        Console.SetCursorPosition(colIni, linIni); Console.Write("+");
+        Console.SetCursorPosition(colIni, linFin); Console.Write("+");
+        Console.SetCursorPosition(colFin, linIni); Console.Write("+");
+        Console.SetCursorPosition(colFin, linFin); Console.Write("+");
     }
-    //metodo para centralizar titulos
 
-    //metodo para mostrar um menu 
- 
+
+    public void centralizar(string texto, int lin, int colIni, int colFin)
+    {
+        int colTexto = ((colFin - colIni - texto.Length) / 2) + colIni;
+        Console.SetCursorPosition(colTexto, lin);
+        Console.Write(texto);
+    }
+
+
+    public string mostrarMenu(List<string> opcoes, int colIni, int linIni)
+    {
+        string opcaoEscolhida = "";
+
+        // procura pela maior largura entre as opções do menu
+        int largura = 0;
+        foreach (string opcao in opcoes)
+        {
+            if (opcao.Length > largura)
+            {
+                largura = opcao.Length;
+            }
+        }
+
+        // define a largura e altura para a moldura do menu
+        int colFin = colIni + largura + 1;
+        int linFin = linIni + opcoes.Count + 2;
+        this.desenharMoldura(colIni, linIni, colFin, linFin);
+
+        // mostra as opções do menu
+        colIni++;
+        linIni++;
+        for (int i = 0; i < opcoes.Count; i++)
+        {
+            Console.SetCursorPosition(colIni, linIni);
+            Console.WriteLine(opcoes[i]);
+            linIni++;
+        }
+
+        // pergunta a opção escolhida
+        Console.SetCursorPosition(colIni, linIni);
+        Console.Write("Escolha uma opção: ");
+        opcaoEscolhida = Console.ReadLine();
+
+        // retorna a opção escolhida
+        return opcaoEscolhida;
+    }
 }
